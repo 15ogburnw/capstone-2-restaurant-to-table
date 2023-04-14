71b3a5d0a87f3465c8fe730d5d12cb83e9b3d58e
wade <15ogburnw@gmail.com>
2023-04-14 00:55:03 -0500

    still a mess with menus database

--- pages/api/user/menus.js
+++ pages/api/user/menus.js
@@ -1,12 +1,12 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 const handler = async (req, res) => {
+  const supabaseServerClient = createServerSupabaseClient({
+    req,
+    res,
+  });
+  const user = await supabaseServerClient.auth.getUser();
   try {
-    const supabaseServerClient = createServerSupabaseClient({
-      req,
-      res,
-    });
-    const user = await supabaseServerClient.auth.getUser();
     if (req.method === "GET") {
       let { data, error } = await supabaseServerClient
         .from("menus")
@@ -16,13 +16,13 @@ const handler = async (req, res) => {
       const menus = data?.map((val) => val.name) || [];
       res.status(200).json({ menus });
     } else if (req.method === "POST") {
-      const { name } = req.body;
+      const { menu } = req.body;
       let { error } = await supabaseServerClient
         .from("menus")
-        .insert({ name, user_id: user.id });
-      if (error) res.status(400).json({ message: error.message });
+        .insert({ name: menu.name, user_id: user.id });
+      if (error) return res.status(400).json({ message: error.message });
       else {
-        res.status(201).json({ message: "Menu successfully created" });
+        return res.status(201).json({ message: "Menu successfully created" });
       }
     } else if (req.method === "DELETE") {
       // TODO: BUILD THIS
