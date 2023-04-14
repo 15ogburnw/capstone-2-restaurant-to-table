ea2f3e1a28bc3f521fc2204075ab86a8e9f8d0b4
wade <15ogburnw@gmail.com>
2023-04-13 23:12:57 -0500

    working on menu api

--- components/Menus/AddMenuModal.js
+++ components/Menus/AddMenuModal.js
@@ -54,92 +54,80 @@ export default function AddMenuModal() {
     }
   );
 
-  const handleSubmit = (e) => {
-    e.preventDefault(e);
-    trigger({ name: newMenuName, user_id: user.id });
-    setShowing(false);
-  };
+  mutate([...data, menuName]);
+  setMenuName("");
+};
 
-  if (error) return (<div></div>)
-  else {
-    return
-    (<div>
-      {showing ?
-        <div
-          ref={modalRef}
-          className=" fixed z-20 m-auto h-[40%] sm:h-1/3 max-w-sm inset-x-0 inset-y-0 p-4 bg-white rounded-lg overflow-hidden shadow-[-6px_6px_20px_2px_rgba(0,0,0,0.3)]  sm:p-6"
+if (error) return <>{console.error(error)}</>;
+if (isLoading)
+  return (
+    <div className="w-screen h-screen flex flex-col bg-white z-10 justify-center items-center text-center">
+      <Loading />
+    </div>
+  );
+return (
+  <div
+    ref={modalRef}
+    className=" fixed z-20 m-auto h-[40%] sm:h-1/3 max-w-sm inset-x-0 inset-y-0 p-4 bg-white rounded-lg overflow-hidden shadow-[-2px_-8px_24px_-6px_rgba(0,0,0,0.3)]  sm:p-6"
+  >
+    <div className="">
+      <div>
+        <div className="mt-4 text-center">
+          <h3
+            className="font-medium leading-6 text-gray-800 capitalize "
+            id="modal-title"
+          >
+            Create a New Menu
+          </h3>
+
+          <p className="mt-2 text-sm text-gray-500 ">
+            Curate a custom collection: Just give your new menu a name and get
+            started organizing your favorite recipes however you like!
+          </p>
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit} className="mt-4">
+        <label
+          className="text-sm text-gray-700 font-bold"
+          htmlFor="menu-name"
         >
-          <div className="">
-            <div className="relative">
-              <XMarkIcon
-                onClick={() => setShowing(false)}
-                className=" hover:cursor-pointer right-1 -mr-1 -mt-3 absolute w-7 h-7 hover:stroke-[3px] stroke-[2px]"
-              ></XMarkIcon>
-              <div className="mt-2 text-center">
-                <h3
-                  className="font-medium leading-6 text-gray-800 "
-                  id="modal-title"
-                >
-                  Create a New Menu
-                </h3>
+          Menu Name
+        </label>
 
-                <p className="mt-1 text-sm text-gray-500 ">
-                  Curate a custom collection: Just give your new menu a name and get
-                  started organizing your favorite recipes however you like!
-                </p>
-              </div>
-            </div>
+        <input
+          type="text"
+          name="menu-name"
+          id="menu-name"
+          placeholder="Enter a menu name"
+          onChange={handleChange}
+          value={menuName}
+          className="flex h-10 px-4  text-sm text-gray-700 bg-white border border-gray-200 rounded-md  w-full mt-2  focus:border-emerald-400 focus:ring-emerald-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+        />
 
-            <form onSubmit={handleSubmit} className="mt-2 flex-auto">
-              <label
-                className="text-sm text-gray-700 font-bold"
-                htmlFor="menu-name"
-              >
-                Menu Name
-              </label>
+        {alert ? (
+          <p classNameName="text-red-500 font-semibold text-center">
+            {alert}
+          </p>
+        ) : null}
 
-              <input
-                type="text"
-                name="menu-name"
-                id="menu-name"
-                placeholder="Enter a menu name"
-                onChange={handleChange}
-                value={newMenuName}
-                className="flex h-10 px-4  text-sm text-gray-700 bg-white border border-gray-200 rounded-md  w-full mt-1  focus:border-emerald-400 focus:ring-emerald-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-              />
+        <div className="sm:mt-6 flex flex-col items-center sm:flex-row  sm:-mx-2">
+          <button
+            onClick={() => setShowModal(false)}
+            className="w-full px-4 py-3 my-2 sm:my-0 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:w-1/2 sm:mx-2  hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
+          >
+            Cancel
+          </button>
 
-              {toast.message && showing ? (
-                <p className="text-red-500 font-semibold sm:-mb-5 mt-1 text-sm text-center">
-                  {toast.message}
-                </p>
-              ) : null}
-
-              <div className="sm:mt-7 flex flex-col items-center sm:flex-row  sm:-mx-2">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setShowing(false);
-                  }}
-                  className="w-full px-4 py-3 my-3 sm:my-0 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-300 rounded-md sm:w-1/2 sm:mx-2  hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
-                >
-                  Cancel
-                </button>
-
-                <button
-                  onClick={handleSubmit}
-                  onMouseEnter={() =>
-                    preload("/api/user/menus", (url) =>
-                      fetch(url).then((res) => res.json())
-                    )
-                  }
-                  className=" w-full px-4 py-3  text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-emerald-400 focus:outline-none focus:ring focus:ring-emerald-300 focus:ring-opacity-40"
-                >
-                  Create Your Menu
-                </button>
-              </div>
-            </form>
-          </div>
-        </div> : null}
-    </div>)
-  }
+          <button
+            type="submit"
+            className=" w-full px-4 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-emerald-500 focus:outline-none focus:ring focus:ring-emerald-300 focus:ring-opacity-40"
+          >
+            Create Your Menu
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+);
 }
