5ea77138bdab82e5d5b74d7ae7e2d00e6729a747
15ogburnw <104091738+15ogburnw@users.noreply.github.com>
2023-04-17 23:18:22 +0000

    still need to fix the modal not saving menus, need to change toasts

--- components/Menus/AddMenuModal.js
+++ components/Menus/AddMenuModal.js
@@ -1,16 +1,22 @@
-import { useRef, useState } from "react";
+import { useState, useRef } from "react";
 
 import useSWR from "swr";
 import useClickOutside from "@/lib/hooks/useClickOutside";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import useSWRMutation from "swr/mutation";
 
-export default function AddMenuModal({ setShowModal }) {
+export default function AddMenuModal() {
   const [newMenuName, setNewMenuName] = useState("");
-  const [alert, setAlert] = useState("");
   const modalRef = useRef(null);
+  const [showModal, setShowModal] = useState(false);
+  const [toast, setToast] = useState({ message: "", type: null });
   useClickOutside(modalRef, setShowModal);
   let { data: menus } = useSWR("/api/user/menus");
+
+  const handleChange = (e) => {
+    const newVal = e.target.value;
+    setNewMenuName(newVal);
+  };
 
   const createMenu = async (url, { name }) => {
     return fetch(url, {
@@ -22,104 +28,116 @@ export default function AddMenuModal({ setShowModal }) {
     }).then((res) => res.json());
   };
 
-  const { trigger } = useSWRMutation("/api/user/menus", createMenu, {
-    optimisticData: (current) => ({ ...current, name: newMenuName }),
-    rollbackOnError: true,
-  });
+  const { trigger, isMutating } = useSWRMutation(
+    "/api/user/menus",
+    createMenu,
+    {
+      optimisticData: (current) => [...current, { name: newMenuName }],
+      rollbackOnError: true,
+      onError: () =>
+        setToast({
+          message:
+            "Something went wrong! Unable to create your menu at this time",
+          type: "danger",
+        }),
+    }
+  );
 
-  const handleChange = (e) => {
-    const newVal = e.target.value;
-    setNewMenuName(newVal);
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault(e);
-    setAlert("");
+    setToast({ message: "", type: null });
     for (let menu of menus) {
       if (menu.name === newMenuName) {
-        setAlert(
-          "You already have a menu with this name, try a different one!"
-        );
-        return;
+        setToast({
+          message:
+            "You already have a menu with this name, try a different one!",
+          type: "danger",
+        });
+      } else if (!newMenuName) {
+        setToast({
+          message: "Please enter a name for your menu!",
+          type: "danger-maybe",
+        });
+      } else {
+        trigger({ name: newMenuName });
+        setNewMenuName("");
+        setToast({ message: "Menu successfully changed!", type: "success" });
+        setShowModal(false);
       }
-    }
-    if (!newMenuName) {
-      setAlert("Please enter a name for your menu!");
-      return;
-    } else {
-      trigger(newMenuName);
-      setNewMenuName("");
-      setShowModal(false);
     }
   };
 
   return (
-    <div
-      ref={modalRef}
-      className=" fixed z-20 m-auto h-[40%] sm:h-1/3 max-w-sm inset-x-0 inset-y-0 p-4 bg-white rounded-lg overflow-hidden shadow-[-6px_6px_20px_2px_rgba(0,0,0,0.3)]  sm:p-6"
-    >
-      <div className="">
-        <div className="relative">
-          <XMarkIcon
-            onClick={() => setShowModal(false)}
-            className=" hover:cursor-pointer right-1 -mr-1 -mt-3 absolute w-7 h-7 hover:stroke-[3px] stroke-[2px]"
-          ></XMarkIcon>
-          <div className="mt-2 text-center">
-            <h3
-              className="font-medium leading-6 text-gray-800 "
-              id="modal-title"
-            >
-              Create a New Menu
-            </h3>
+    <>
+      {showModal ? (
+        <div
+          ref={modalRef}
+          className=" fixed z-20 m-auto h-[40%] sm:h-1/3 max-w-sm inset-x-0 inset-y-0 p-4 bg-white rounded-lg overflow-hidden shadow-[-6px_6px_20px_2px_rgba(0,0,0,0.3)]  sm:p-6"
+        >
+          <div className="">
+            <div className="relative">
+              <XMarkIcon
+                onClick={() => setShowModal(false)}
+                className=" hover:cursor-pointer right-1 -mr-1 -mt-3 absolute w-7 h-7 hover:stroke-[3px] stroke-[2px]"
+              ></XMarkIcon>
+              <div className="mt-2 text-center">
+                <h3
+                  className="font-medium leading-6 text-gray-800 "
+                  id="modal-title"
+                >
+                  Create a New Menu
+                </h3>
 
-            <p className="mt-1 text-sm text-gray-500 ">
-              Curate a custom collection: Just give your new menu a name and get
-              started organizing your favorite recipes however you like!
-            </p>
+                <p className="mt-1 text-sm text-gray-500 ">
+                  Curate a custom collection: Just give your new menu a name and
+                  get started organizing your favorite recipes however you like!
+                </p>
+              </div>
+            </div>
+
+            <form onSubmit={handleSubmit} className="mt-2 flex-auto">
+              <label
+                className="text-sm text-gray-700 font-bold"
+                htmlFor="menu-name"
+              >
+                Menu Name
+              </label>
+
+              <input
+                type="text"
+                name="menu-name"
+                id="menu-name"
+                placeholder="Enter a menu name"
+                onChange={handleChange}
+                value={newMenuName}
+                className="flex h-10 px-4  text-sm text-gray-700 bg-white border border-gray-200 rounded-md  w-full mt-1  focus:border-emerald-400 focus:ring-emerald-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+              />
+
+              {toast.message ? (
+                <p className="text-red-500 font-semibold sm:-mb-5 mt-1 text-sm text-center">
+                  {toast.message}
+                </p>
+              ) : null}
+
+              <div className="sm:mt-7 flex flex-col items-center sm:flex-row  sm:-mx-2">
+                <button
+                  onClick={() => setShowModal(false)}
+                  className="w-full px-4 py-3 my-3 sm:my-0 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-300 rounded-md sm:w-1/2 sm:mx-2  hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
+                >
+                  Cancel
+                </button>
+
+                <button
+                  type="submit"
+                  className=" w-full px-4 py-3  text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-emerald-400 focus:outline-none focus:ring focus:ring-emerald-300 focus:ring-opacity-40"
+                >
+                  Create Your Menu
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-
-        <form onSubmit={handleSubmit} className="mt-2 flex-auto">
-          <label
-            className="text-sm text-gray-700 font-bold"
-            htmlFor="menu-name"
-          >
-            Menu Name
-          </label>
-
-          <input
-            type="text"
-            name="menu-name"
-            id="menu-name"
-            placeholder="Enter a menu name"
-            onChange={handleChange}
-            value={newMenuName}
-            className="flex h-10 px-4  text-sm text-gray-700 bg-white border border-gray-200 rounded-md  w-full mt-1  focus:border-emerald-400 focus:ring-emerald-300 focus:ring-opacity-40  focus:outline-none focus:ring"
-          />
-
-          {alert ? (
-            <p className="text-red-500 font-semibold sm:-mb-5 mt-1 text-sm text-center">
-              {alert}
-            </p>
-          ) : null}
-
-          <div className="sm:mt-7 flex flex-col items-center sm:flex-row  sm:-mx-2">
-            <button
-              onClick={() => setShowModal(false)}
-              className="w-full px-4 py-3 my-3 sm:my-0 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-300 rounded-md sm:w-1/2 sm:mx-2  hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
-            >
-              Cancel
-            </button>
-
-            <button
-              type="submit"
-              className=" w-full px-4 py-3  text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-emerald-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-emerald-400 focus:outline-none focus:ring focus:ring-emerald-300 focus:ring-opacity-40"
-            >
-              Create Your Menu
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+      ) : null}
+    </>
   );
 }
