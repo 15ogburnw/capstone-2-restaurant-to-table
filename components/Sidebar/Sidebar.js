aa3de28543b3bf4855f47f3e45f7396c44e2197a
wade <15ogburnw@gmail.com>
2023-04-17 06:15:09 -0500

    got menu modal working

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,5 +1,3 @@
-// RIGHT NOW IF YOU CLICK ANYWHERE ON THE SCREEN THE MODAL FOR MAKING A MENU DISAPPEARS,
-
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -9,6 +7,7 @@ import {
   RectangleStackIcon,
   ArrowRightOnRectangleIcon,
   Cog6ToothIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/24/outline";
 import SVG from "react-inlinesvg";
 import { useRouter } from "next/router";
@@ -16,37 +15,21 @@ import SideBarMenuItem from "../Menus/SidebarMenuItem";
 import AddMenuModal from "../Menus/AddMenuModal";
 import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import { faSquarePlus as faSquarePlusSolid } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import useSWR from "swr";
+import useSWR, { mutate, preload } from "swr";
 import Loading from "../Loading";
 
 export default function Sidebar() {
-  const { data, isLoading, error } = useSWR("/api/user/menus");
+  const { data: menus, isLoading, error } = useSWR("/api/user/menus");
 
   const router = useRouter();
-  //get current user's menus
-  const { data: menus, isLoading } = useSWR("/api/user/menus");
-
-  // initialize client for supabase
-  const supabase = useSupabaseClient();
-
-  // possible colors for list of user menus in sidebar
-  const COLORS = [
-    "bg-red-500",
-    "bg-orange-500",
-    "bg-yellow-500",
-    "bg-emerald-500",
-    "bg-blue-500",
-    "bg-purple-500",
-    "bg-pink-500",
-  ];
-
-  // hover state for button that opens modal
-  const [hoverModalBtn, setHoverModalBtn] = useState(false);
-
-  // reference modal and set it to close if the user clicks outside its bounds
+  const [hoverMenu, setHoverMenu] = useState(false);
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    preload("/api/user/menus", (url) => fetch(url).then((res) => res.json()));
+  }, []);
 
   const COLORS = [
     "bg-red-500",
@@ -66,24 +49,17 @@ export default function Sidebar() {
     }
   };
 
+  const clearCache = () => mutate(() => true, undefined, { revalidate: false });
+
   const handleSignOut = (e) => {
     e.preventDefault();
-
-    // clears the swr cache
-    const signOut = async () => {
+    async function signOut() {
       const { error } = await supabase.auth.signOut();
-      if (error) throw new Error();
-
-      // clear the SWR cache
-      await mutate(() => true, undefined, { revalidate: false });
-      router.push("/landing");
-    };
-
+      router.push("/");
+      if (error) console.error(error);
+    }
     signOut();
-  };
-
-  const handleOpenModal = (e) => {
-    setShowModal(true);
+    clearCache();
   };
 
   // TODO:
@@ -91,17 +67,12 @@ export default function Sidebar() {
   // --FIX LOGO - TRADE FOR LOGO WITH FULL NAME AND CENTER ABOVE AVATAR (FIGURE OUT WHY THE CURRENT LOGO IS FAILING TO LOAD SOMETIMES)
   // --Style this error message with a toast or something
   if (error) return <h1>Something Went Wrong! {console.error(error)}</h1>;
-  else if (isLoading)
-    return (
-      <div className="h-screen w-screen flex flex-col justify-center items-center absolute bg-white z-10">
-        <Loading />
-      </div>
-    );
+
   return (
     <aside className="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto  border-r border-gray-300 ">
       <Link href="/dashboard">
         <SVG
-          src="/img/rtt-logos/rtt-icon.svg"
+          src="/rtt-logos/rtt-icon.svg"
           className="h-8 w-auto inline mr-3"
           alt="logo"
         />
@@ -229,15 +200,12 @@ export default function Sidebar() {
             <h2 className="text-base font-semibold text-gray-800 ">My Menus</h2>
             <div
               onMouseEnter={() => {
-                setHoverModalBtn(true);
-                preload("/api/user/menus", (url) =>
-                  fetch(url).then((res) => res.json())
-                );
+                setHoverMenu(true);
               }}
-              onMouseLeave={() => setHoverModalBtn(false)}
+              onMouseLeave={() => setHoverMenu(false)}
               onClick={() => setShowModal(true)}
             >
-              {hoverModalBtn ? (
+              {hoverMenu ? (
                 <FontAwesomeIcon
                   icon={faSquarePlusSolid}
                   className="font-normal h-5 w-5 text-emerald-600 flex-none cursor-pointer"
@@ -252,28 +220,36 @@ export default function Sidebar() {
           </div>
 
           <nav className="mt-4 mx-3 space-y-3 ">
-            {data ? (
+            {menus?.length > 0 && !isLoading ? (
               <>
-                {data?.menus?.map((val, idx, newArr) => {
-                  let color = getColor(idx);
+                {menus.map((menu, idx) => {
                   return (
-                    <SideBarMenuItem key={val} dotColor={color} name={val} />
+                    <SideBarMenuItem
+                      key={menu.name}
+                      dotColor={getColor(idx)}
+                      name={menu.name}
+                    />
                   );
                 })}
               </>
-            ) : (
-              console.log(data)
-            )}
+            ) : null}
+
+            {!menus || menus.length === 0 ? (
+              <button className="flex justify-between w-full px-3 py-2 text-sm font-medium text-gray-600  duration-300 transform rounded-lg ">
+                <div className="flex items-center gap-x-2 ">
+                  <span>
+                    You don&apos;t have any menus yet! Create your first one to
+                    get started
+                  </span>
+                </div>
+              </button>
+            ) : null}
+            {isLoading ? <Loading /> : null}
           </nav>
         </div>
       </div>
-      <ClientOnlyPortal selector="#modals">
-        {showModal && (
-          <div id="modal-wrapper">
-            <AddMenuModal setShowModal={setShowModal} />
-          </div>
-        )}
-      </ClientOnlyPortal>
+
+      {showModal ? <AddMenuModal setShowModal={setShowModal} /> : null}
     </aside>
   );
 }
--- pages/api/user/menus.js
+++ pages/api/user/menus.js
@@ -1,52 +1,76 @@
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 
 const handler = async (req, res) => {
-  // console.log(req);
-  const supabaseServerClient = createServerSupabaseClient({
-    req,
-    res,
-  });
-  const resp = await supabaseServerClient.auth.getSession();
-  const { name, id } = req.body;
+  try {
+    const supabaseServerClient = createServerSupabaseClient({
+      req,
+      res,
+    });
+    const {
+      data: { user },
+    } = await supabaseServerClient.auth.getUser();
+    const { name, id } = req.body;
+    let data, error;
 
-  // case "POST":
-  //   menus = await supabaseServerClient
-  //     .from("menus")
-  //     .insert({ name: name, user_id: user.id })
-  //     .select("id, name");
-  //   if (menus.error)
-  //     res.status(menus.status).json({ error: menus.statusText });
-  //   else {
-  //     console.log("menu successfully added:", menus.data);
-  //     res.status(201).json(menus.data);
-  //   }
-  //   break;
+    switch (req.method) {
+      case "GET":
+        ({ data, error } = await supabaseServerClient
+          .from("menus")
+          .select("name, id"));
+        if (error) {
+          res.status(error.code).send({ error: error.message });
+        } else {
+          res.status(200).json(data);
+        }
+        break;
 
-  // case "DELETE":
-  //   menus = await supabaseServerClient
-  //     .from("menus")
-  //     .delete()
-  //     .eq("name", name)
-  //     .select("id", "name");
-  //   if (menus.error)
-  //     return res.status(menus.status).json({ error: menus.statusText });
-  //   else {
-  //     console.log("deleted menu", menus.data);
-  //     return res.status(200).json(menus.data);
-  //   }
+      case "POST":
+        ({ data, error } = await supabaseServerClient
+          .from("menus")
+          .insert({ name: name, user_id: user.id })
+          .select("name, id"));
+        if (error) res.status(error.code).json({ error: error.message });
+        else {
+          console.log("menus after post", data);
+          res.status(201).json(data);
+        }
+        break;
 
-  // case "PATCH":
-  //   menus = await supabaseServerClient
-  //     .from("menus")
-  //     .update({ name: name })
-  //     .eq("id", id)
-  //     .select("id, name");
-  //   if (menus.error)
-  //     return res.status(menus.status).json({ error: menus.statusText });
-  //   else {
-  //     console.log("menu after update", menus.data);
-  //     return res.status(200).json(menus.data);
-  //   }
+      case "DELETE":
+        ({ data, error } = await supabaseServerClient
+          .from("menus")
+          .delete()
+          .eq("name", name)
+          .select("id", "name"));
+        if (error) res.status(error.code).send({ error: error.message });
+        else {
+          console.log("deleted menu", data);
+          res
+            .status(200)
+            .json({ message: `successfully deleted menu "${name}"` });
+        }
+        break;
+
+      case "PATCH":
+        ({ data, error } = await supabaseServerClient
+          .from("menus")
+          .update({ name })
+          .eq("id", id)
+          .select("id", "name"));
+        if (error) res.status(error.code).json({ error: error.message });
+        else {
+          console.log("menu after update", data.name);
+        }
+        break;
+
+      default:
+        res
+          .status(500)
+          .send({ error: "Something went wrong, please try again later" });
+    }
+  } catch (error) {
+    res.status(error.code).send(error.message);
+  }
 };
 
 export default handler;
