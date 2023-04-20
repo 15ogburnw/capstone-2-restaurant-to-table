5ffc98e2d65f6abe5202293b903bd5a75f66dc80
wade <15ogburnw@gmail.com>
2023-04-20 06:38:32 -0500

    fixed a lot of middleware and routing bugs

---
+++ components/LoadingButton.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+
+export default function LoadingButton({ originalButton }) {
+  const classes = originalButton.classes;
+  console.log(classes);
+
+  return (
+    <>
+      <button
+        type="button"
+        className={`${classes} disabled disabled:bg-emerald-200 disabled:border-emerald-200 sm:w-55 sm-`}
+      >
+        <FontAwesomeIcon icon={faCircleNotch} spin />
+        <span className="text-emerald-400 font-semibold">Loading...</span>
+      </button>
+      )
+    </>
+  );
+}
