1e12ef76a9790c668d9bfc9e5121416ff323ee73
15ogburnw <104091738+15ogburnw@users.noreply.github.com>
2023-04-28 03:08:19 +0000

    Pending changes exported from your codespace

--- components/Forms/SignupForm.js
+++ components/Forms/SignupForm.js
@@ -49,23 +49,20 @@ export default function SignupForm() {
   ) => {
     const { email, password } = values;
 
-    const { error } = await supabase.auth.signUp(
-      {
-        email,
-        password,
-      },
-      {
-        options: {
-          redirectTo: "/dashboard",
-        },
-      }
-    );
-
+    const { data, error } = await supabaseClient.auth.signUp({
+      email,
+      password,
+      // options: { redirectTo: () => getAuthRedirectURL() },
+    });
     if (error) {
       setErrorMessage("Something went wrong! Please try again");
       console.error(error);
       console.log(error.message);
+      setLoading(false)
     }
+    if (data) console.log(data);
+
+    setLoading(false);
   };
 
   return (
