2f76bc2d9df2fe663d5019b0914522dcebe5bbe3
Wade <15ogburnw@gmail.com>
2023-05-04 04:16:46 -0500

    can login and query the database finally
    Signed-off-by: Wade <15ogburnw@gmail.com>

--- components/Forms/SignupForm.js
+++ components/Forms/SignupForm.js
@@ -6,6 +6,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/fontawesome-free";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons"
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
 
