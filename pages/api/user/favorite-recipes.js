93e28d4938a980c81b41b157800774bf93063d47
15ogburnw <104091738+15ogburnw@users.noreply.github.com>
2023-05-16 20:25:26 +0000

    reconciling local branch with codespace

--- pages/api/user/favorite-recipes.js
+++ pages/api/user/favorite-recipes.js
@@ -2,86 +2,86 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
 const handler = async (req, res) => {
-	const supabaseServerClient = createServerSupabaseClient({
-		req,
-		res,
-	});
-	const {
-		data: { user },
-		error,
-	} = await supabaseServerClient.auth.getUser();
-	const { recipe_id } = req.body;
+  const supabaseServerClient = createServerSupabaseClient({
+    req,
+    res,
+  });
+  const {
+    data: { user },
+    error,
+  } = await supabaseServerClient.auth.getUser();
+  const { recipe_id } = req.body;
 
-	if (error) return res.status(error.status).json({ error: error.message });
-	let favorites;
+  if (error) return res.status(error.status).json({ error: error.message });
+  let favorites;
 
-	switch (req.method) {
-		case "GET":
-			/**If a GET request is sent to this endpoint, get all of the current user's favorite recipes.
-			 * Since we have Row-level-security enabled, the logged-in user will only see their own favorites, so we can just grab all values matching
-			 * an entry on the recipes table by querying the join table.
-			 *  TODO: **Test this logic**
-			 */
-			favorites = await supabaseServerClient
-				.from("favorite_recipes")
-				.select("recipe_id(id,name)");
-			if (favorites.error) {
-				console.log(favorites.error)
-				return res
-					.status(favorites.error.code)
-					.json({ message: favorites.error.message });
-			}
-			else {
+  switch (req.method) {
+    case "GET":
+      /**If a GET request is sent to this endpoint, get all of the current user's favorite recipes.
+       * Since we have Row-level-security enabled, the logged-in user will only see their own favorites, so we can just grab all values matching
+       * an entry on the recipes table by querying the join table.
+       *  TODO: **Test this logic**
+       */
+      favorites = await supabaseServerClient
+        .from("favorite_recipes")
+        .select("recipe_id(id,name)");
+      if (favorites.error) {
+        console.log(favorites.error)
+        return res
+          .status(favorites.error.code)
+          .json({ message: favorites.error.message });
+      }
+      else {
 
-				favorites = favorites.data?.map((val) => val.recipe_id) || [];
-				console.log(favorites)
-				console.log('The user has these favorite recipes:', favorites);
-				return res.status(200).json(favorites);
-			}
+        favorites = favorites.data?.map((val) => val.recipe_id) || [];
+        console.log(favorites)
+        console.log('The user has these favorite recipes:', favorites);
+        return res.status(200).json(favorites);
+      }
 
-		case "POST":
+    case "POST":
 
-			/** If a post request is sent to this endpoint, add the recipe to the join table representing the user's favorite recipes.
-			 * This will not work if the recipe is not present in the database, so we perform an upsert on the recipes table first. Throw an error
-			 * if we run into difficulties.
-			 * 
-			 * TODO: **add upsert logic for this and test the endpoint**
-			 */
+      /** If a post request is sent to this endpoint, add the recipe to the join table representing the user's favorite recipes.
+       * This will not work if the recipe is not present in the database, so we perform an upsert on the recipes table first. Throw an error
+       * if we run into difficulties.
+       * 
+       * TODO: **add upsert logic for this and test the endpoint**
+       */
 
-			favorites = await supabaseServerClient
-				.from("favorite_recipes")
-				.insert({ recipe_id, user_id: user.id })
-				.select("recipe_id(id,name)");
-			if (favorites.error)
-				return res
-					.status(favorites.error.code)
-					.json({ message: favorites.error.message });
-			else {
-				console.log("recipe successfully added to favorites:", favorites.data);
-				return res.status(201).json(favorites.data);
-			}
+      favorites = await supabaseServerClient
+        .from("favorite_recipes")
+        .insert({ recipe_id, user_id: user.id })
+        .select("recipe_id(id,name)");
+      if (favorites.error)
+        return res
+          .status(favorites.error.code)
+          .json({ message: favorites.error.message });
+      else {
+        console.log("recipe successfully added to favorites:", favorites.data);
+        return res.status(201).json(favorites.data);
+      }
 
-		case 'DELETE':
-			favorites = await supabaseServerClient
-				.from('favorite_recipes')
-				.delete()
-				.eq('recipe_id', recipe_id)
-				.select('recipe_id', 'name');
-			if (favorites.error)
-				return res
-					.status(favorites.error.code)
-					.json({ message: favorites.error });
-			else {
-				console.log(
-					'recipe successfully removed from favorites',
-					favorites.data
-				);
-				return res.status(200).json(favorites.data);
-			}
+    case "DELETE":
+      favorites = await supabaseServerClient
+        .from("favorite_recipes")
+        .delete()
+        .eq("recipe_id", recipe_id)
+        .select("recipe_id");
+      if (favorites.error)
+        return res
+          .status(favorites.error.code)
+          .json({ error: favorites.error });
+      else {
+        console.log(
+          "recipe successfully removed from favorites",
+          favorites.data
+        );
+        return res.status(200).json(favorites.data);
+      }
 
-		default:
-			return res.status(400).json('Bad Request');
-	}
+    default:
+      return res.status(400).json("Bad Request");
+  }
 };
 
 export default handler;
